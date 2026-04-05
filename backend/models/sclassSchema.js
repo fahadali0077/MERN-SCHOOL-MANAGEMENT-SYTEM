122aaddc1ee043dc9b453d495922a3f9e7e549ce
Fahad Ali <fahadj698@gmail.com>
2026-04-05 19:08:32 +0500

    Delete backend directory

--- backend/models/sclassSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const sclassSchema = new mongoose.Schema({
-    sclassName: { type: String, required: true },
-    school:     { type: mongoose.Schema.Types.ObjectId, ref: 'admin', required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('sclass', sclassSchema);
